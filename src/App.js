537149b61f33bcb278da6893ae03159ad3444bc5
Alex Moriarty <almoriar@iu.edu>
2018-06-21 12:59:32 -0400

    add test path with routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route, Switch, NavLink } from 'react-router-dom'
 
 import Header from './Header'
 
@@ -8,10 +9,21 @@ class App extends Component {
   render() {
    
     return (
-      
       <div className="App">
       <Header />
+      <div>
+        <ul>
+          <li>
+            <NavLink to="/test"> testLink </NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/test" render={() => <h1> TEST </h1>}/>
+          <Route render={() => <p> to get started click above </p>} />
+        </Switch>
       </div>
+      </div>
+
     )
   }
 }
